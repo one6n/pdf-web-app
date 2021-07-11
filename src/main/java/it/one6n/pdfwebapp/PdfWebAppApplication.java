dac5c9e4c278373264e3a99f8e6fe6f02e8964fd
one6n <clod94@live.it>
2021-07-11 13:00:57 +0200

    first commit

---
+++ src/main/java/it/one6n/pdfwebapp/PdfWebAppApplication.java
@@ -0,0 +1,13 @@
+package it.one6n.pdfwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/one6n/pdfwebapp/PdfWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package it.one6n.pdfwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
