43515049ad9f6d67c23ed7b12dd419bc3d511f2e
one6n <clod94@live.it>
2021-07-17 20:19:34 +0200

    initial stub for split document.

--- src/main/java/it/one6n/pdfwebapp/PdfService.java
+++ src/main/java/it/one6n/pdfwebapp/PdfService.java
@@ -1,9 +1,13 @@
 package it.one6n.pdfwebapp;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.sql.Blob;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import javax.sql.rowset.serial.SerialBlob;
 import javax.sql.rowset.serial.SerialException;
@@ -13,6 +17,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import it.one6n.pdfutils.PdfUtils;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
@@ -39,10 +44,6 @@ public class PdfService {
 		}
 	}
 
-	public PdfPojo findPdfById(Long id) {
-		return getPdfRepo().findById(id).orElse(null);
-	}
-
 	public PdfPojo buildPdfPojo(MultipartFile file) throws IOException, SerialException, SQLException {
 		PdfPojo pdf = new PdfPojo();
 		byte[] barr = file.getBytes();
@@ -52,6 +53,10 @@ public class PdfService {
 		pdf.setNumberOfPages(getNumberOfPages(barr));
 		pdf.setInsertDate(new Date());
 		return pdf;
+	}
+
+	public PdfPojo findPdfById(Long id) {
+		return getPdfRepo().findById(id).orElse(null);
 	}
 
 	public byte[] deserialize(Blob data) throws SQLException {
@@ -64,5 +69,28 @@ public class PdfService {
 
 	public int getNumberOfPages(byte[] barr) throws IOException {
 		return PDDocument.load(barr).getNumberOfPages();
+	}
+
+	public List<Long> splitDocuments(PdfPojo pdf, int delimiter) throws SQLException, IOException {
+		List<Long> ids = null;
+		if (pdf != null) {
+			ids = new ArrayList<>(0);
+			byte[] barr = deserialize(pdf.getData());
+			PDDocument docOriginal = PDDocument.load(barr);
+			List<PDDocument> splitted = PdfUtils.splitDocument(docOriginal, delimiter);
+			int counter = 1;
+			for (PDDocument splittedDocument : splitted) {
+				PdfPojo pojo = new PdfPojo();
+				pojo.setFilename(pdf.getFilename() + "_" + counter);
+				pojo.setNumberOfPages(splittedDocument.getNumberOfPages());
+				pojo.setInsertDate(new Date());
+				OutputStream os = new ByteArrayOutputStream();
+				/*
+				 * byte[] barr = new byte[]; splittedDocument.save(os); os.write(barr);
+				 */
+				++counter;
+			}
+		}
+		return ids;
 	}
 }
--- src/main/java/it/one6n/pdfwebapp/RestWebController.java
+++ src/main/java/it/one6n/pdfwebapp/RestWebController.java
@@ -1,8 +1,10 @@
 package it.one6n.pdfwebapp;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,26 +17,31 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/rest")
 public class RestWebController {
 
-	public static final String SPLIT_FILE_PATH = "/splitFile/{id}";
+	public static final String SPLIT_FILE_PATH = "/splitFile";
 
 	@Autowired
 	private PdfService pdfService;
 
 	@PostMapping(path = SPLIT_FILE_PATH, produces = "application/json")
-	public RestResult splitFile(@PathVariable Long id) {
-		log.debug("id={}", id);
+	public RestResult splitFile(@RequestBody Map<String, String> input) {
+		log.debug("input={}", input);
 		RestResult result = new RestResult(false);
-		if (id != null) {
-			PdfPojo pdf = getPdfService().findPdfById(id);
-			if (pdf != null) {
-				// result.setData(pdf);
-				try {
-					log.debug("File loaded: filename={}, size={}", pdf.getFilename(), pdf.getData().length());
-				} catch (Exception e) {
-					throw new RuntimeException(e);
+		if (input != null) {
+			try {
+				Long id = Long.parseLong(input.get("id"));
+				int splitIndex = Integer.parseInt(input.get("splitIndex"));
+				PdfPojo pdf = getPdfService().findPdfById(id);
+				if (pdf != null) {
+					// result.setData(pdf);
+					if (pdf.getNumberOfPages() > splitIndex) {
+						log.debug("File loaded: filename={}, size={}", pdf.getFilename(), pdf.getData().length());
+
+					}
 				}
-				result.setResult(true);
+			} catch (Exception e) {
+				throw new RuntimeException(e);
 			}
+			result.setResult(true);
 		}
 		return result;
 	}
