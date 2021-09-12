6669e85310a293cc0133a686c1e6bf3f1f09b044
one6n <clod94@live.it>
2021-09-12 12:51:22 +0200

    added more methods to BaseMongoService

--- src/main/java/it/one6n/pdfwebapp/services/BaseMongoService.java
+++ src/main/java/it/one6n/pdfwebapp/services/BaseMongoService.java
@@ -1,15 +1,21 @@
 package it.one6n.pdfwebapp.services;
 
+import java.io.InputStream;
 import java.util.Set;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.gridfs.GridFsTemplate;
 import org.springframework.stereotype.Service;
 
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.gridfs.GridFSBucket;
 import com.mongodb.client.gridfs.GridFSBuckets;
+import com.mongodb.client.gridfs.GridFSFindIterable;
+import com.mongodb.client.gridfs.model.GridFSFile;
+import com.mongodb.client.gridfs.model.GridFSUploadOptions;
+import com.mongodb.client.model.Filters;
 import com.mongodb.client.result.DeleteResult;
 
 import lombok.Getter;
@@ -20,12 +26,12 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 public class BaseMongoService {
 
-	@Value("${mongo.database.name}")
-	private String dbName;
 	@Autowired
 	private MongoClient mongoClient;
 	@Autowired
 	private MongoTemplate mongoTemplate;
+	@Autowired
+	private GridFsTemplate gridFsTemplate;
 
 	public Set<String> getCollections() {
 		Set<String> collections = getMongoTemplate().getCollectionNames();
@@ -36,8 +42,16 @@ public class BaseMongoService {
 		getMongoTemplate().createCollection(collectionName);
 	}
 
+	public <T> void dropCollection(Class<T> entityClass) {
+		getMongoTemplate().dropCollection(entityClass);
+	}
+
+	public void dropCollection(String collectionName) {
+		getMongoTemplate().dropCollection(collectionName);
+	}
+
 	public GridFSBucket findOrCreateBucket(String bucketName) {
-		return GridFSBuckets.create(getMongoClient().getDatabase(getDbName()), bucketName);
+		return GridFSBuckets.create(getMongoTemplate().getDb(), bucketName);
 	}
 
 	/**
@@ -52,6 +66,17 @@ public class BaseMongoService {
 	}
 
 	/**
+	 * This method perform an insert operation in the collection of the given
+	 * domainType. Use saveDocument for upsert.
+	 * 
+	 * @param obj Object to save
+	 * @return the inserted object
+	 */
+	public Object insert(Object obj) {
+		return getMongoTemplate().insert(obj);
+	}
+
+	/**
 	 * This method perform an upsert operation.
 	 * 
 	 * @param obj            Object to save
@@ -62,7 +87,61 @@ public class BaseMongoService {
 		return getMongoTemplate().save(obj, collectionName);
 	}
 
+	/**
+	 * This method perform an upsert operation in the collection of the given
+	 * domainType.
+	 * 
+	 * @param obj Object to save
+	 * @return the inserted object
+	 */
+	public Object save(Object obj) {
+		return getMongoTemplate().save(obj);
+	}
+
 	public DeleteResult delete(Object obj, String collectionName) {
 		return getMongoTemplate().remove(obj, collectionName);
+	}
+
+	public ObjectId uploadFileFromInputStream(String bucketName, String filename, InputStream is,
+			GridFSUploadOptions gridFsOptions) {
+		GridFSBucket bucket = findOrCreateBucket(bucketName);
+		if (gridFsOptions != null)
+			return bucket.uploadFromStream(filename, is, gridFsOptions);
+		else
+			return uploadFileFromInputStream(bucketName, filename, is);
+	}
+
+	public ObjectId uploadFileFromInputStream(String bucketName, String filename, InputStream is) {
+		GridFSBucket bucket = findOrCreateBucket(bucketName);
+		return bucket.uploadFromStream(filename, is, null);
+	}
+
+	public InputStream getInputStreamById(String bucketName, ObjectId id) {
+		GridFSBucket bucket = findOrCreateBucket(bucketName);
+		return bucket.openDownloadStream(id);
+	}
+
+	public InputStream getInputStreamByFilename(String bucketName, String filename) {
+		GridFSBucket bucket = findOrCreateBucket(bucketName);
+		return bucket.openDownloadStream(filename);
+	}
+
+	public void deleteFileById(String bucketName, ObjectId id) {
+		GridFSBucket bucket = findOrCreateBucket(bucketName);
+		bucket.delete(id);
+	}
+
+	/**
+	 * This method delete from the given bucket all the files with the given
+	 * filename.
+	 * 
+	 * @param bucketName name of the bucket where perform the operation
+	 * @param filename   name of the bucket where perform the operation
+	 */
+	public void deleteFilesByFilename(String bucketName, String filename) {
+		GridFSBucket bucket = findOrCreateBucket(bucketName);
+		GridFSFindIterable files = bucket.find(Filters.eq("filename", filename));
+		for (GridFSFile file : files)
+			bucket.delete(file.getId());
 	}
 }
