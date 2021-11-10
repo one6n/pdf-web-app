7729a3737a620296ddcbdef5fd26a68da23ebf67
one6n <clod94@live.it>
2021-11-10 22:43:52 +0100

    moved PdfUtils method in PdfUtils library

--- src/main/java/it/one6n/pdfwebapp/services/PdfService.java
+++ src/main/java/it/one6n/pdfwebapp/services/PdfService.java
@@ -1,14 +1,12 @@
 package it.one6n.pdfwebapp.services;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.pdmodel.PDPage;
 import org.springframework.stereotype.Service;
 
+import it.one6n.pdfutils.PdfUtils;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -17,36 +15,15 @@ import lombok.Setter;
 @Service
 public class PdfService {
 
-	// Pdfbox utility methods
 	public int getNumberOfPages(byte[] barr) throws IOException {
-		int numberOfPages = 0;
-		try (PDDocument doc = PDDocument.load(barr)) {
-			numberOfPages = getNumberOfPages(doc);
-		}
-		return numberOfPages;
+		return PdfUtils.getNumberOfPages(barr);
 	}
 
 	public int getNumberOfPages(PDDocument document) throws IOException {
-		return document.getNumberOfPages();
+		return PdfUtils.getNumberOfPages(document);
 	}
 
 	public List<PDDocument> splitDocument(PDDocument document, int index) throws IOException {
-		List<PDDocument> splitted = null;
-		Iterator<PDPage> iterator = document.getPages().iterator();
-		int i = 0;
-		splitted = new ArrayList<>();
-		PDDocument doc1 = new PDDocument();
-		while (i < index && iterator.hasNext()) {
-			doc1.addPage(iterator.next());
-			++i;
-		}
-		splitted.add(doc1);
-		PDDocument doc2 = new PDDocument();
-		while (iterator.hasNext()) {
-			doc2.addPage(iterator.next());
-			++i;
-		}
-		splitted.add(doc2);
-		return splitted;
+		return PdfUtils.splitDocument(document, index);
 	}
 }
